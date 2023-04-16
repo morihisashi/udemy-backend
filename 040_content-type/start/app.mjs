3f94d544ec23c17d2bbe9833d1bfe328b0e3f08d
morihisashi <hisa.1995.322@outlook.com>
2023-04-16 22:04:23 +0900

    content-typeについて学ぼう

--- 040_content-type/start/app.mjs
+++ 040_content-type/start/app.mjs
@@ -1,9 +1,30 @@
 import * as http from "http";
 
+// content-type: 
+// ブラウザとサーバーが通信しているコンテンツの
+// フォーマットと文字コードの情報
+
+// content-type: text/html; charset=UTF-8
+
 const server = http.createServer(function(req, res) {
     console.log(req.url);
+    // res.writeHead(200, {'content-type':'text/html; charset=UTF-8'});
     if(req.url === '/hello') {
-        res.end('<script>window.alert("frontend")</script>');
+        res.end(`<!DOCTYPE html>
+        <html lang="en">
+        
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        
+        <body>
+            <h1>こんにちは</h1>
+        </body>
+        
+        </html>`)
     } else if(req.url === '/bye') {
         res.end('bye');
     }
