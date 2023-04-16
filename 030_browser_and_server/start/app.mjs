91bc9c37ef8b2418b70cf02fcba7c5b8c26707fb
morihisashi <hisa.1995.322@outlook.com>
2023-04-16 21:51:07 +0900

    jsの実行環境とnode環境の違いについて

--- 030_browser_and_server/start/app.mjs
+++ 030_browser_and_server/start/app.mjs
@@ -6,7 +6,7 @@ import * as http from "http";
 const server = http.createServer(function(req, res) {
     console.log(req.url);
     if(req.url === '/hello') {
-        res.end('hello');
+        res.end('<script>window.alert("frontend")</script>');
     } else if(req.url === '/bye') {
         res.end('bye');
     }
