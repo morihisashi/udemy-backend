5cf53d00f3727e59df507cd8746bb55683e47c97
morihisashi <hisa.1995.322@outlook.com>
2023-04-16 22:44:32 +0900

    HTTPについて3

--- 060_http_method/start/app.mjs
+++ 060_http_method/start/app.mjs
@@ -19,17 +19,27 @@ const server = http.createServer(function (req, res) {
     res.end(`
       <form action="/result" method="POST">
         <input type="text" name="title">
+        <input type="text" name="description">
         <input type="submit">
       </form>
     `);
   } else {
-    // console.log(req.url);
-    // console.log(req.method);
+    console.log(req.url);
+    console.log(req.method);
     if(req.method === "GET"){
       // GETのパラメータを取得
       const queryString = req.url.split("?")[1];
       const params = new URLSearchParams(queryString);
       console.log(params);
+    } else if(req.method === "POST") {
+      let data = "";
+      req.on("data", function (chunk) {
+        data += chunk;
+      });
+      req.on("end", function() {
+        const params = new URLSearchParams(data);
+        console.log(params);
+      })
     }
     res.end(req.url);
   }
