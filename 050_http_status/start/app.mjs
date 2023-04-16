e20a5e684c03382beeb384b9dda2832027817647
morihisashi <hisa.1995.322@outlook.com>
2023-04-16 22:14:00 +0900

    HTTPステータスコードについて

--- 050_http_status/start/app.mjs
+++ 050_http_status/start/app.mjs
@@ -1,12 +1,28 @@
 import * as http from "http";
 
+// HTTP status codes:
+// レスポンスの状態を表すコード
+// 200番台: 成功
+//  - コンテンツ作成成功：201、その他の処理成功：200
+// 300番台: リダイレクト
+//  - 恒久：301, 一時：302, キャッシュ利用：304
+// 400番台: クライアントエラー
+//  - 認証失敗：401, アクセス禁止：403, Not Found：404
+// 500番台: サーバーエラー
+//  - 不明なエラー：500、サービスダウン：503
+
+// see https://developer.mozilla.org/ja/docs/Web/HTTP/Status
+
 const server = http.createServer(function(req, res) {
     console.log(req.url);
-    res.writeHead(200, {'content-type': 'text/html; charset=UTF-8'});
+    
     if(req.url === '/hello') {
-        res.end(`<h1>こんにちは</h1>`)
+        res.writeHead(302, {location: '/redirected'});
+        res.end(`<h1>こんにちは</h1>`);
     } else if(req.url === '/bye') {
         res.end('bye');
+    } else {
+        res.end(req.url);
     }
 });
 
