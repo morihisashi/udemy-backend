62911a63b7c051c71e76887729546148eb44e2f0
morihisashi <hisa.1995.322@outlook.com>
2023-04-19 22:26:11 +0900

    jsonの渡し方

--- 090_json/start/app.mjs
+++ 090_json/start/app.mjs
@@ -4,9 +4,10 @@ import express from 'express';
 const PORT = 8080;
 const app = express();
 
+
 app.get('/', function(req, res) {
-    res.send('hello');
-})
+    res.json({ message: 'hello', number: 1, array: ['banana', 'orange', 1] });
+});
 
 app.listen(PORT, function() {
     console.log(`Server start: http://localhost:${PORT}`)
