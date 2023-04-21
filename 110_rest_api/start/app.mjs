e077bc3d9d9e905a0b28673742433f2d82038c74
morihisashi <hisa.1995.322@outlook.com>
2023-04-21 20:25:03 +0900

    APIについて

--- 110_rest_api/start/app.mjs
+++ 110_rest_api/start/app.mjs
@@ -15,6 +15,32 @@ app.get('/products', function (req, res) {
   res.json(products);
 });
 
+app.post('/create-product', function (req, res) {
+  const newProduct = req.body;
+  products.push(newProduct);
+  console.log(products);
+  res.json(newProduct);
+});
+
+app.post('/delete-product', function (req, res) {
+  const deleteId = req.body.id;
+  products.splice(deleteId,1);
+  console.log(products);
+  res.json({ deleteId });
+});
+
+app.post('/update-product', function (req, res) {
+  const targetProduct = products[req.body.id];
+  if(req.body.hasOwnProperty('name')) {
+    targetProduct.name = req.body.name;
+  }
+  if(req.body.hasOwnProperty('price')) {
+    targetProduct.price = req.body.price;
+  }
+  console.log(products);
+  res.json(targetProduct);
+});
+
 app.listen(PORT, function () {
   console.log(`Server start: http://localhost:${PORT}`);
 });
