97eb050edbfd852f286f824dad8f9a243b70a063
morihisashi <hisa.1995.322@outlook.com>
2023-04-23 19:28:52 +0900

    ミドルウェアの仕組みについて1

--- 120_use/start/app.mjs
+++ 120_use/start/app.mjs
@@ -4,7 +4,7 @@ import express from 'express';
 # useとexpress
 ## ルートハンドラとミドルウェア
 - request -> (middleware) -> route handler
-- use vs get (部分一致 vs 完全一致)
+- use vs get (前方一致 vs 完全一致)
 
 ## next()の使い方
 - nextの必要性
@@ -20,13 +20,25 @@ app.use(express.json());
 
 // ミドルウェア：ルートハンドラの前後に行われる処理
 app.use('/', function(req, res, next) {
+  console.log('/ start');
+  next();
+});
 
+app.use('/', function(req, res, next) {
+  console.log('/ middle');
+  next();
 });
 
 // ルートハンドラ：パスとメソッドに紐付くメインの処理
-app.get('/', function(req, res) {
-
+app.get('/*', function(req, res, next) {
+  console.log('/ get');
+  next();
 });
+
+app.get('/api', function(req, res, next) {
+  console.log('/ get2');
+});
+
 app.listen(PORT, function () {
   console.log(`Server start: http://localhost:${PORT}`);
 });
